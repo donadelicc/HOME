2ed8d18101bd5bec7028e771e556136799a1d413
Preben Andersen <preb1anders1@gmail.com>
2023-08-25 00:21:25 +0200

    added hugginface symbol

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import {
   AiOutlineGithub,
   AiOutlineTwitter,
   AiOutlineLinkedin,
-  AiOutlineYoutube,
+  AiOutlineSmile,
 } from "react-icons/ai"
 
 const Footer = () => {
@@ -12,17 +12,17 @@ const Footer = () => {
       <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
       <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
         <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
-          © 2023 Hosna Qasmei<a href="/" className="hover:underline"></a>
+          © 2023 Preben Andersen<a href="/" className="hover:underline"></a>
         </div>
         <div className="flex flex-row items-center justify-center space-x-2 mb-1">
-          <a href="https://github.com/hqasmei" rel="noreferrer" target="_blank">
+          <a href="https://github.com/donadelicc" rel="noreferrer" target="_blank">
             <AiOutlineGithub
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
               size={30}
             />
           </a>
           <a
-            href="https://twitter.com/hqasmei"
+            href="https://twitter.com/preb1_anders1"
             rel="noreferrer"
             target="_blank"
           >
@@ -33,7 +33,7 @@ const Footer = () => {
           </a>
 
           <a
-            href="https://www.linkedin.com/in/hosnaqasmei/"
+            href="https://www.linkedin.com/in/preben-andersen0/"
             rel="noreferrer"
             target="_blank"
           >
@@ -43,11 +43,11 @@ const Footer = () => {
             />
           </a>
           <a
-            href="https://www.youtube.com/channel/UCQBMkSDgbxDb8usMeXmOZyA"
+            href="https://huggingface.co/donadelicc"
             rel="noreferrer"
             target="_blank"
           >
-            <AiOutlineYoutube
+            <AiOutlineSmile
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
               size={30}
             />
